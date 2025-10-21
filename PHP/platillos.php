331fe8ead88b832fb1e7f4162505c18818992a7e
Janettth <jalu040803@gmail.com>
2025-10-21 13:05:40 -0600

    archivo actualizado

--- PHP/platillos.php
+++ PHP/platillos.php
@@ -1,17 +1,6 @@
 <?php
 include("../PHP/conexion.php");
 
-if (isset($_POST["nombre"])) {
-    $nombre = $_POST["nombre"];
-    $ingredientes = $_POST["ingredientes"];
-    $preparacion = $_POST["preparacion"];
-    $origen = $_POST["origen"];
-
-    $conexion->query("INSERT INTO platillos (Nombre, Ingredientes, Preparacion, Origen)
-                      VALUES ('$nombre', '$ingredientes', '$preparacion', '$origen')");
-    echo "<p>✅ Platillo agregado correctamente.</p>";
-}
-
 $resultado = $conexion->query("SELECT * FROM platillos");
 ?>
 
@@ -23,16 +12,6 @@ $resultado = $conexion->query("SELECT * FROM platillos");
   <link rel="stylesheet" href="../CSS/estiloTablas.css">
 </head>
 <body>
-
-  <!--
-  <h2>Agregar nuevo platillo</h2>
-  <form method="POST">
-    <input type="text" name="nombre" placeholder="Nombre" required>
-    <input type="text" name="ingredientes" placeholder="Ingredientes">
-    <input type="text" name="preparacion" placeholder="Preparación">
-    <input type="text" name="origen" placeholder="Origen">
-    <button type="submit">Guardar</button>
-  </form>-->
 
   <h2>Patillos en la base de datos</h2>
   <table border="0">
